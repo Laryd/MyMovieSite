691c2eaa0e91df0e9ee6948dc56b74d17bfab080
hillarymuboka <hillarymuboka@gmail.com>
2025-07-06 16:38:00 +0300

    build home page with hero and trending movie/TV rows

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,39 @@
+import HeroSection from './components/HeroSection'
+import MediaRow from './components/MediaRow'
+import {
+  getTrending,
+  getPopularMovies,
+  getTopRatedMovies,
+  getPopularTV,
+  getOnAirTV,
+  getNowPlaying,
+} from './lib/tmdb'
 
-import AppContainer from './components/app-container'
+export default async function HomePage() {
+  const [trending, popular, topRated, popularTV, onAir, nowPlaying] = await Promise.all([
+    getTrending(),
+    getPopularMovies().then(d => d.results),
+    getTopRatedMovies().then(d => d.results),
+    getPopularTV().then(d => d.results),
+    getOnAirTV(),
+    getNowPlaying(),
+  ])
 
-export default function Home() {
+  const hero = trending[0]
+  const rest = trending.slice(1).filter(i => i.media_type !== 'person')
+
   return (
-    <>
-      <AppContainer />
-    </>
-  );
+    <div className="pb-16">
+      {hero && <HeroSection item={hero} />}
+
+      <div className="space-y-12 mt-12">
+        <MediaRow title="Trending This Week" items={rest} viewAllHref="/movies" />
+        <MediaRow title="Now Playing in Theaters" items={nowPlaying} type="movie" />
+        <MediaRow title="Popular Movies" items={popular} type="movie" viewAllHref="/movies" />
+        <MediaRow title="Top Rated Movies" items={topRated} type="movie" />
+        <MediaRow title="Popular TV Shows" items={popularTV} type="tv" viewAllHref="/tv" />
+        <MediaRow title="Currently Airing" items={onAir} type="tv" viewAllHref="/tv" />
+      </div>
+    </div>
+  )
 }
